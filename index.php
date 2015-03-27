d3905b05debf84c90279d26e3a50b932d32f6b97
galymzhan91 <g.makhaliev@gmail.com>
2015-03-27 14:30:36 +0600

    index.php
    This is example project

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+include ('db.php');
+?> 
+<html>
+<head>
+
+</head>
+<body>
+	<table style="border:solid 1px">
+		<tr> <td>Login Page</td> </tr>
+		<tr> <td><input type="text" name="username" placeholder="username"></td> </tr>
+		<tr> <td><input type="text" name="password" placeholder="password"></td> </tr>
+		<tr> <td><input type="submit" name="button" value="ok"></td> <td><a href="register.php">register</a></td></tr>
+	</table>
+</body>
+</html>
