d566ca50e5dbc297f359140695b21b359035dcdd
galymzhan91 <g.makhaliev@gmail.com>
2015-03-29 00:33:06 +0600

    Update register.php

--- register.php
+++ register.php
@@ -3,16 +3,18 @@
 	
 	if(isset($_POST['username']) && isset($_POST['password']) && isset($_POST['name']) && isset($_POST['birth'])){
 		if(!empty($_POST['username']) && !empty($_POST['password']) && !empty($_POST['name']) && !empty($_POST['birth'])){
-			$username = $_POST['username'];
-			$password = $_POST['password'];
-			$fullname = $_POST['name'];
-			$date = $_POST['birth'];
+			$username = mysql_real_escape_string($_POST['username']);
+			$password = mysql_real_escape_string($_POST['password']);
+			$fullname = mysql_real_escape_string($_POST['name']);
+			$date = mysql_real_escape_string($_POST['birth']);
+			
 			$query = 'INSERT INTO xusers(id,username,password,fullname,birth) 
 							values(null, "'.$username.'", "'.$password.'", "'.$fullname.'", "'.$date.'")';
 			$result = mysql_query($query) or die('Wrong query: '.mysql_error());
-			echo "Information was stored!";
+			header("Location: ".$_SERVER['REQUEST_URI']);
 		}
 	}
+	
 ?>
 <html>
 <head>
@@ -23,8 +25,8 @@
 	<form method="POST" action="register.php">
 	<table style="border:solid 1px">
 		<tr><td><h4>Register Page</h4></td> <td></td> </tr>
-		<tr><td>Username: </td> <td><input type="text" name="username" placeholder="username"></td></tr>
-		<tr><td>Password: </td> <td><input type="text" name="password" placeholder="password"></td></tr>
+		<tr><td>Username: </td> <td><input type="text" name="username" placeholder="username" required></td></tr>
+		<tr><td>Password: </td> <td><input type="text" name="password" placeholder="password" required></td></tr>
 		<tr><td>FullName: </td> <td><input type="text" name="name" placeholder="name"></td></tr>
 		<tr><td>Birthday: </td> <td><input type="date" name="birth"></td></tr>
 		<tr><td><input type="submit" name="submit" value="submit"></td><td><a href="index.php">login page</a></td></tr>
@@ -32,3 +34,4 @@
 	</form>
 </body>
 </html>
+
