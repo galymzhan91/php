225fcf77e5e81c85745ae3c3af2526227daf8e78
galymzhan91 <g.makhaliev@gmail.com>
2015-03-29 22:24:58 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,22 @@
 <?php
 include ('db.php');
-
+if(isset($_POST['button'])){
+	$username = $_POST['username'];
+	$password = $_POST['password'];
+	
+	$query = 'SELECT * from users WHERE username='.$username.' AND password='.$password;
+	$login = mysql_query($query);
+	
+}
 ?> 
 <html>
 <head>
 	<meta http-equiv="content-type" content="text/html" />
     <meta charset="UTF-8" />
+	<title>Login page</title>
 </head>
 <body>
+<form method="post" action="index.php" name="login_form">
 	<table style="border:solid 1px">
 		<tr> <td><h4>Login Page</h4></td> </tr>
 		<tr> <td><input type="text" name="username" placeholder="username" required></td> </tr>
@@ -15,5 +24,6 @@ include ('db.php');
 		<tr> <td><input type="submit" name="button" value="ok">   <a href="register.php">register</a></td></tr>
 
 	</table>
+</form>
 </body>
 </html>
